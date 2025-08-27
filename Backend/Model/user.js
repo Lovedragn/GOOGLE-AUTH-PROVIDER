f68e80884a7bb3a2002836406da6eb5bbcebeeb4
Sujith <sujith.sappani@gmail.com>
2025-08-27 19:23:00 +0530

    change in json

--- Backend/Model/user.js
+++ Backend/Model/user.js
@@ -9,8 +9,8 @@ const supabase = createClient(
 
 export const gettasks = async (req, res) => {
   try {
-    const {sub} = req.body;
-    
+    const { sub } = req.body;
+
     const { data, error } = await supabase
       .from("tasks")
       .select("*")
@@ -20,6 +20,59 @@ export const gettasks = async (req, res) => {
     if (error) throw Error("user get error");
     return res.json(data);
   } catch (err) {
+    console.log("error");
     return res.status(500).json({ message: "Failed to fetch user data" });
+  }
+};
+export const addtasks = async (req, res) => {
+  try {
+    const { sub, values } = req.body;
+    const date = new Date().toISOString().split("T")[0];
+
+    const { data: existing, error: fetcherror } = await supabase
+      .from("tasks")
+      .select("*")
+      .eq("sub", sub)
+      .maybeSingle  ();
+
+    if (fetcherror) throw fetcherror;
+
+    if (existing) {
+      const ensureArray = (val) => Array.isArray(val) ? val : [];
+
+      const updatedTitle = [...ensureArray(existing.title), values.title];
+      const updatedDesc = [...ensureArray(existing.desc), values.desc];
+      const updatedDate = [...ensureArray(existing.date), date];
+
+      const { error: updateError } = await supabase
+        .from("tasks")
+        .update({
+          title: updatedTitle,
+          desc: updatedDesc,
+          date: updatedDate,
+        })
+        .eq("sub", sub);
+
+      if (updateError) throw updateError;
+
+      return res.status(200).json({ message: "Appended successfully" });
+    }
+
+    const { data, error } = await supabase
+      .from("tasks")
+      .insert([
+        {
+          sub: sub,
+          title: [values.title],
+          desc: [values.desc],
+          date: [date],
+        },
+      ]);
+    if (error) throw error;
+
+    return res.status(200).json({ message: "Inserted new task", data });
+  } catch (err) {
+    console.error("Upload error:", err);
+    return res.status(500).json({ message: "failed", error: err.message });
   }
 };
--- Backend/Server.js
+++ Backend/Server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import token from "./token.js";
-import { gettasks } from "./Model/user.js";
+import { gettasks , addtasks } from "./Model/user.js";
 
 
 dotenv.config();
@@ -13,7 +13,8 @@ app.use(cors());
 app.use(express.json());
 
 app.post("/auth/login", token); // Google OAuth login
-app.post("/db/tasks", gettasks); // fetch tasks
+app.post("/db/get/tasks", gettasks); // fetch tasks
+app.post("/db/add/tasks" ,addtasks );
 
 
 app.listen(5000, () => console.log("Server is running on port 5000"));
